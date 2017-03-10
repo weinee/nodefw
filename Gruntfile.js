ac898118953c99bfd44c1e543641669f88c103f5
weineeL <LiJF_wn@163.com>
2017-03-10 13:02:05 +0800

    在grunt中使用babelel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,14 @@
 
 module.exports = function(grunt) {
 
+  /// 配置插件选项
   grunt.initConfig(require('./tasks/options')(grunt));
 
 /** 所有tasks下的目录引用都是相对于Gruntfile所在目录的 */
   /// load-grunt-tasks插件会从package.json中查找所有的 'grunt-' 开头的依赖并加载到grunt
   /// 批量执行grunt.loadNpmTasks()方法，
   require('load-grunt-tasks')(grunt);
+
   // 引用tasks目录，注册所有自定义任务
   grunt.loadTasks('./tasks');
 
@@ -18,7 +20,7 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-concat');
 
 /// 以下代码被移到tasks目录下
-/// 注册任务序列
+/// 注册任务序列, 添加任务
   // grunt.registerTask('test', ['jshint', 'qunit']);
   // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
 };

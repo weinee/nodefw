51f8c42d660a06f2de273325202d7af06efad0c1
weineel <LiJF_wn@163.com>
2017-03-13 21:18:11 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-
+import {red} from 'ansicolors';
 import Hapi from 'hapi';
+import {join} from 'path';
 
 const server = new Hapi.Server({
 
@@ -10,10 +11,31 @@ server.connection({
 	port: 8000
 });
 
+// 注册插件
+server.register([
+    require('vision'),
+    require('inert')
+], (err) => {
+    if (err) {
+        console.log(red('error'), '注册出错', err.message);
+        throw err;
+    }
+
+    // console.log('jade view path', join(__dirname, 'views'));
+    // 初始化
+    server.views({
+        engines: { jade: require('jade')},
+        path: join(__dirname, 'views')
+    });
+
+});
+
+// 添加扩展，监听全局事件
 server.ext('onRequest', (request, reply) => {
-	request.setUrl('/happy');
+	// request.setUrl('/happy');
+    // console.log(red("error")); // 显示红色 log
 	return reply.continue();
-})
+});
 
 server.route({
     method: 'GET',
@@ -29,6 +51,48 @@ server.route({
     path:'/hello', 
     handler: function (request, reply) {
         return reply('hello world');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/views/jindex',
+    handler: function (req, reply) {
+        return reply.view('jindex');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/views/index',
+    handler: function (req, reply) {
+        return reply.view('index2222');
+    }
+});
+
+
+// 静态文件 inert
+server.route({
+    method: 'GET',
+    path: '/asset/{param*}',
+    handler: {
+        directory: {
+            path: join(__dirname, 'asset'), // 文件路径
+            redirectToSlash: true,
+            index: true
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/html/{param*}',
+    handler: {
+        directory: {
+            path: join(__dirname, 'views'), // 文件路径
+            redirectToSlash: true,
+            index: true
+        }
     }
 });
 
